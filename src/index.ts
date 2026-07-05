0e2eb7e447f5503076dd5d2cd23fcef83598da88
some-du6e <103129948+some-du6e@users.noreply.github.com>
2026-07-05 13:24:43 -0600

    feat(init) init slack bolt app

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { app } from "./slack"
+
+app.start(process.env.PORT || 3000);
---
+++ src/slack.ts
@@ -0,0 +1,22 @@
+import { App } from '@slack/bolt';
+
+/**
+ * This sample Slack application uses Socket Mode.
+ * For the companion getting started setup guide, see:
+ * https://docs.slack.dev/tools/bolt-js/getting-started/
+ */
+
+// Initializes your app with your bot token and app token
+export const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN
+});
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say({
+    text: `Hey there!`
+  });
+});
